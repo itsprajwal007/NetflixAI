36e87aac75370538fcaca3a82ca911f499c2fa32
Prajwal Gaikwad <gaikwadprajwal81@gmail.com>
2024-10-24 22:42:41 +0530

    Made a design a lot more responsive

--- src/components/GPTSearch.js
+++ src/components/GPTSearch.js
@@ -4,13 +4,15 @@ import GPTSearchBar from "./GPTSearchBar";
 
 const GPTSearch = () => {
   return (
-    <div>
-      <div className="fixed -z-10">
-        <img src={BG_URL} alt="logo" />
+    <>
+      <div className="fixed inset-0 z-[-1]">
+        <img className="w-full h-full object-cover" src={BG_URL} alt="logo" />
       </div>
-      <GPTSearchBar />
-      <GPTMovieSuggestions />
-    </div>
+      <div>
+        <GPTSearchBar />
+        <GPTMovieSuggestions />
+      </div>
+    </>
   );
 };
 
--- src/components/VideoTitle.js
+++ src/components/VideoTitle.js
@@ -1,13 +1,13 @@
 export const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="w-screen aspect-video pt-[20%]  px-24 absolute text-white bg-gradient-to-r from-black">
-      <h1 className="text-6xl font-bold">{title}</h1>
-      <p className="p-6 text-lg w-1/2 ">{overview}</p>
-      <div>
-        <button className="bg-white text-xl text-black p-4 px-12 rounded-lg hover:bg-opacity-80">
+    <div className="w-screen aspect-video pt-[20%] px-6  md:px-24 absolute text-white bg-gradient-to-r from-black">
+      <h1 className="text-2xl md:text-6xl font-bold">{title}</h1>
+      <p className="hidden md:inline-block py-6 text-lg w-1/4 ">{overview}</p>
+      <div className="my-4 md:m-0">
+        <button className="bg-white text-xl text-black py-1 md:py-4 px-3 md:px-12 rounded-lg hover:bg-opacity-80">
           ▶ Play
         </button>
-        <button className="bg-gray-500 mx-2 text-xl text-white p-4 px-12 bg-opacity-50 rounded-lg hover:bg-opacity-80">
+        <button className="hidden md:inline-block my-2 bg-gray-500 mx-2 text-xl text-white p-4 px-12 bg-opacity-50 rounded-lg hover:bg-opacity-80">
           More Info
         </button>
       </div>
